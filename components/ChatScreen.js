b9e42451529cf087e815ec61007dfba2513b5a31
kanishka-28 <kanishka.gour.mat20@itbhu.ac.in>
2021-12-01 23:41:08 +0530

    bugs solved

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -1,15 +1,16 @@
 import firebase from "firebase";
-import React, { useRef, useState} from 'react'
+import React, { useRef, useState } from 'react'
 import { useCollection } from 'react-firebase-hooks/firestore'
 import { HiUserCircle, HiOutlineDotsVertical } from 'react-icons/hi'
-import { ImAttachment,ImMic, ImPointLeft } from 'react-icons/im'
+import { ImAttachment, ImMic, ImPointLeft } from 'react-icons/im'
 import { CgSmileMouthOpen } from 'react-icons/cg'
 import { auth, db } from '../firebase'
 import Message from './Message'
 import { useAuthState } from "react-firebase-hooks/auth";
 import getRecipientEmail from "../utils/chats";
 import { useRouter } from "next/dist/client/router";
-const ChatScreen = ({ messages, chat , screen}) => {
+import TimeAgo from "timeago-react";
+const ChatScreen = ({ messages, chat, screen }) => {
 
     const [user] = useAuthState(auth);
     const [input, setinput] = useState('')
@@ -23,27 +24,28 @@ const ChatScreen = ({ messages, chat , screen}) => {
     const photo = recipientSnapShot?.docs?.[0]?.data()?.photo;
     const recipient = recipientSnapShot?.docs?.[0]?.data()
     const name = recipientSnapShot?.docs?.[0]?.data()?.name;
+    const lastseen = recipientSnapShot?.docs?.[0]?.data()?.lastseen.toDate();
     const email = getRecipientEmail(user, chat.users);
     const endOfMessagesRef = useRef(null)
 
-    const scrollToBottom=()=>{
+    const scrollToBottom = () => {
         endOfMessagesRef.current.scrollIntoView({
             behavior: "smooth",
             block: "start"
         })
     }
-    const sendMessage=(e)=>{
-        document.getElementById( 'bottom' ).scrollIntoView();
-        e.preventDefault();    
-        if(input===''){
+    const sendMessage = (e) => {
+        document.getElementById('bottom').scrollIntoView();
+        e.preventDefault();
+        if (input === '') {
             return null
         }
-            //update the last seen
+        //update the last seen
         db.collection('users').doc(user.uid).set(
             {
                 lastseen: firebase.firestore.FieldValue.serverTimestamp(),
             },
-            {merge: true}
+            { merge: true }
         )
         db.collection('chats').doc(chat.id).collection('messages').add({
             timestamp: firebase.firestore.FieldValue.serverTimestamp(),
@@ -55,27 +57,36 @@ const ChatScreen = ({ messages, chat , screen}) => {
         scrollToBottom();
     }
 
-    const ShowMessages=()=>{
-        if(messageSnapShot){
-            return messageSnapShot.docs.map((message)=>(
+    const ShowMessages = () => {
+        scrollToBottom();
+        if (messageSnapShot) {
+            return messageSnapShot.docs.map((message) => (
                 <Message key={message.id} userName={message.data().user} message={{
                     ...message.data(),
                     timestamp: message.data().timestamp?.toDate().getTime()
-                }}/>
+                }} />
             ))
-        }else{
+        } else {
             return null
         }
     }
     return (
         <div className="flex flex-col justify-between bg-pink-100 h- lg:w-3/4 md:w-2/3 w-full" id="bottom" style={{ backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/b/b1/Little_background.jpg")` }}>
             <nav className="sticky top-0 bg-white p-3 px-6 flex justify-between items-center">
-                {screen==="mobile" && <ImPointLeft className="w-6 h-5 mr-2 cursor-pointer" onClick={()=>router.push("/")}/>}
+                {screen === "mobile" && <ImPointLeft className="w-6 h-5 mr-2 cursor-pointer" onClick={() => router.push("/")} />}
                 <div className="flex gap-4 items-center">
-                    {photo?<img src={photo} alt="profile" className="w-10 h-10 rounded-full" />:<HiUserCircle className="w-10 h-10 cursor-pointer hover:opacity-80" />}
+                    {photo ? <img src={photo} alt="profile" className="w-10 h-10 rounded-full" /> : <HiUserCircle className="w-10 h-10 cursor-pointer hover:opacity-80" />}
                     <div>
-                        <p className="font-bold text-l">{name?name:email}</p>
-                        <p className="text-gray-600 text-xs">Last Seen : {recipient?recipient.lastseen.seconds:'unavailable'}</p>
+                        <p className="font-bold text-l">{name ? name : email}</p>
+                        {/* <p className="text-gray-600 text-xs">Last Seen : {recipient?recipient.lastseen.seconds:'unavailable'}</p> */}
+                        <div className="text-md flex gap-1  text-gray-500" >
+                            Last Active:
+                            {
+                                lastseen ?
+                                    <TimeAgo datetime={lastseen} /> :
+                                    <h4 className="" > Not available</h4>
+                            }
+                        </div>
                     </div>
                 </div>
                 <div className="flex gap-5 justify-evenly items-center">
@@ -83,15 +94,15 @@ const ChatScreen = ({ messages, chat , screen}) => {
                     <HiOutlineDotsVertical className="w-5 h-5 mx-2 cursor-pointer" />
                 </div>
             </nav>
-            <div className="bg-red-"><ShowMessages/></div>
+            <div className="bg-red-"><ShowMessages /></div>
             <div className="h-16" ref={endOfMessagesRef}></div>
             <footer className="sticky bottom-0 bg-gray-300 p-3 px-10 flex justify-between z-30">
-            <form className="flex gap-5 items-center w-full">
-                <CgSmileMouthOpen className="w-6 h-6"/>
-                <input value={input} className="md:w-full w-3/4 p-4 rounded-md" onChange={(e)=>setinput(e.target.value)}/>
-                <ImMic className="w-6 h-6 rounded-full text-"/>
-                <button type="submit" className="bg-green-500 font-bold px-2 py-1 rounded-md" onClick={sendMessage} >Send</button>
-            </form>
+                <form className="flex gap-5 items-center w-full">
+                    <CgSmileMouthOpen className="w-6 h-6" />
+                    <input value={input} className="md:w-full w-3/4 p-4 rounded-md" onChange={(e) => setinput(e.target.value)} />
+                    <ImMic className="w-6 h-6 rounded-full text-" />
+                    <button type="submit" className="bg-green-500 font-bold px-2 py-1 rounded-md" onClick={sendMessage} >Send</button>
+                </form>
             </footer>
         </div>
     )
